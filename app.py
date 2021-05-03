5e631e25ae8e599891ad72d7e5f7579f078bdfe5
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-03 07:56:57 -0400

    Removed ReDirect From App.py

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template("redirect.html")
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template("index.html")
 
