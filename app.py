c8e8f72172587c36cde7fb31ff7c07d7ae03b277
Neil Shah <neildshah1013@gmail.com>
2021-06-01 17:49:37 -0400

    Added Login With Discord

--- app.py
+++ app.py
@@ -1,54 +1,44 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, session
 import os
+from oauth import Oauth
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] ="347834y7tureiwfhbjvfwkehuiergy"
 
-NAME = os.environ["NAME"]
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", discord_url=Oauth.discord_login_url)
 
-@app.route('/features')
-def features():
-    return render_template("features.html")
-
-@app.route('/statistics')
-def statistics():
-    return render_template("statistics.html")
 
 @app.route('/invite')
 def invite():
     return render_template("invite.html")
-
-@app.route('/commands')
-def commands():
-    return render_template("commands.html")
 
 
 @app.route('/privacy')
 def terms():
     return render_template("privacy.html")
 
-@app.route("/rickroll")
-def rickroll():
-    return render_template("rickroll.html")
  
 @app.route('/support')
 def support():
     return render_template('support.html')
 
-@app.route('/owner')
-def ownerFlase():
-    return render_template('invite.html')
+@app.route('/login')
+def login():
+    code = request.args.get("code")
+    at = Oauth.get_access_token(code)
+    session["token"] = at
 
+    user = Oauth.get_user_json(at)
+    user_name, user_discrim, user_avatar, user_id = user.get("username"), user.get("discriminator"), user.get("avatar"), user.get("id")
+    user_avatar_url = f"https://cdn.discordapp.com/avatars/{user_id}/{user_avatar}.png"
+    user_discrim = str(user_discrim)
+    userName = user_name + "#" + user_discrim
+    print(userName)
+    return render_template('dashboard.html', user_avatar_url=user_avatar_url, user_tag=userName)
 
-@app.route('/owner/<name>')
-def owner(name=None):
-    if name == NAME:
-        return render_template('owner.html')
-    else:
-        return render_template('invite.html')
 
 @app.errorhandler(404)
 def page_no_found(e):
